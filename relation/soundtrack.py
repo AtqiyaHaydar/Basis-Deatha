ebdd071ec997d716e0bec64dd57779bc6da0b199
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-04 01:24:39 +0700

    feat: soundtrack class

--- relation/soundtrack.py
+++ relation/soundtrack.py
@@ -2,6 +2,19 @@ from Seeding import Seeding
 
 class SoundTrack(Seeding):
   def __init__(self, connection):
+    self.app_ids = []
     super().__init__(connection)
   
-  
+  def seeding(self, num_records):
+    self.app_ids = self.get_all_app_id()
+
+    for _ in range(num_records):
+      soundtrackID = self.app_ids[self.random.randint(0, len(self.app_ids - 1))]
+      durasi_total_lagu = self.fake.random_int(30, 300)
+
+      sql = f"INSERT INTO SOUNDTRACK (soundtrackID, durasi_total_lagu) VALUES (%s, %s)"
+
+      val = (soundtrackID, durasi_total_lagu)
+
+      self.cursor.execute(sql, val)
+      self.connection.commit()
