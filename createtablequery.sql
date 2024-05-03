d29decea9ca76a32f31339003aad55d2bba2b908
AtqiyaHaydar <ashford1524@gmail.com>
2024-05-03 20:50:10 +0700

    fix: create table

--- createtablequery.sql
+++ createtablequery.sql
@@ -1,17 +1,16 @@
-CREATE TABLE Apps (
-    app_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
-    developer_id INTEGER NOT NULL,
+CREATE TABLE Apps(
+    appID INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+    devID INTEGER NOT NULL,
     judul VARCHAR(50) NOT NULL,
-    harga CHAR(10) NOT NULL DEFAULT '0',
     tanggal_peluncuran date,
-    rating INTEGER CHECK (rating >= 0 AND rating <= 5) DEFAULT 0,
-    ukuran FLOAT,
+    ukuran NUMBER,
     deskripsi VARCHAR(255),
-    FOREIGN KEY (developer_id) REFERENCES Developer(developer_id) ON DELETE RESTRICT,
+    harga CHAR(10) NOT NULL DEFAULT '0',
+    FOREIGN KEY (devID) REFERENCES Developer(devID) ON DELETE RESTRICT,
 );
 
 CREATE TABLE Developer(
-    developer_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+    devID INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
     email VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -22,7 +21,7 @@ CREATE TABLE Developer(
 )
 
 CREATE TABLE User(
-    user_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+    userID INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
     email VARCHAR(255) NOT NULL,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -30,108 +29,125 @@ CREATE TABLE User(
     nama_belakang VARCHAR(255) DEFAULT "",
     tanggal_lahir date,
     usia INTEGER,
-    level INTEGER,
+    level INTEGER DEFAULT 0,
     balance FLOAT DEFAULT 0.0,
 )
 
 CREATE TABLE MemilikiAplikasi(
-    user_id INTEGER NOT NULL 
-    app_id INTEGER NOT NULL,
+    userID INTEGER NOT NULL 
+    appID INTEGER NOT NULL,
     total_waktu FLOAT DEFAULT 0,
     waktu_terakhir date,
     jumlah_achievement INTEGER DEFAULT 0,
     rating INTEGER CHECK(rating >= 0 and rating <= 5) DEFAULT 0,
-    PRIMARY KEY (user_id, app_id),
-    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (app_id) REFERENCES Apps(app_id) ON DELETE CASCADE,
+    PRIMARY KEY (userID, appID),
+    FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE,
+    FOREIGN KEY (appID) REFERENCES Apps(appID) ON DELETE CASCADE,
 )
 
 CREATE TABLE SoundTrack(
-    soundtrack_id INTEGER PRIMARY KEY NOT NULL,
+    soundtrackID INTEGER PRIMARY KEY NOT NULL,
     durasi_total_lagu FLOAT DEFAULT 0 NOT NULL,
-    FOREIGN KEY (soundtrack_id) REFERENCES Apps(app_id) ON DELETE CASCADE
+    FOREIGN KEY (soundtrackID) REFERENCES Apps(appID) ON DELETE CASCADE
 )
 
 CREATE TABLE VideoGames(
-    game_id INTEGER PRIMARY KEY NOT NULL, 
-    FOREIGN KEY (game_id) REFERENCES Apps(app_id) ON DELETE CASCADE
+    gameID INTEGER PRIMARY KEY NOT NULL, 
+    FOREIGN KEY (gameID) REFERENCES Apps(appID) ON DELETE CASCADE
 )
 
 CREATE TABLE MenggunakanLagu(
-    soundtrack_id INTEGER NOT NULL,
-    lagu_id INTEGER NOT NULL,
-    PRIMARY KEY (soundtrack_id, lagu_id),
-    FOREIGN KEY (soundtrack_id) REFERENCES SoundTrack(soundtrack_id) ON DELETE CASCADE,
-    FOREIGN KEY (lagu_id) REFERENCES Lagu(lagu_id) ON DELETE CASCADE
+    soundtrackID INTEGER NOT NULL,
+    laguID INTEGER NOT NULL,
+    PRIMARY KEY (soundtrackID, laguID),
+    FOREIGN KEY (soundtrackID) REFERENCES SoundTrack(soundtrackID) ON DELETE CASCADE,
+    FOREIGN KEY (laguID) REFERENCES Lagu(laguID) ON DELETE CASCADE
 )
 Create TABLE Lagu(
-    lagu_id INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+    laguID INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
     judul_lagu VARCHAR(30) NOT NULL,
     durasi_lagu FLOAT DEFAULT 0 NOT NULL,
 )
 
 CREATE TABLE Follow(
-    developer_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    PRIMARY KEY (developer_id, user_id),
-    FOREIGN KEY (developer_id) REFERENCES Developer(developer_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,
+    developerID INTEGER NOT NULL,
+    userID INTEGER NOT NULL,
+    PRIMARY KEY (developerID, userID),
+    FOREIGN KEY (developerID) REFERENCES Developer(developerID) ON DELETE CASCADE,
+    FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE,
 )
 
 CREATE TABLE Pertemanan(
-    user_id INTEGER NOT NULL,
-    user_id2 INTEGER NOT NULL,
+    user1ID INTEGER NOT NULL,
+    user2ID INTEGER NOT NULL,
     status_pertemanan ENUM("FRIENDS", "PENDING", "BLOCKED"),
-    PRIMARY KEY (user_id, user_id2),
-    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id2) REFERENCES User(user_id) ON DELETE CASCADE,
+    PRIMARY KEY (user1ID, user2ID),
+    FOREIGN KEY (user1ID) REFERENCES User(userID) ON DELETE CASCADE,
+    FOREIGN KEY (user2ID) REFERENCES User(userID) ON DELETE CASCADE,
 )
+
 CREATE TABLE Forum(
-    forum_id INTEGER NOT NULL AUTOINCREMENT,
-    user_id INTEGER NOT NULL,
+    forumID INTEGER NOT NULL AUTOINCREMENT,
     judul VARCHAR(255) NOT NULL,
     waktu_pembuatan_vorum date NOT NULL,
-    PRIMARY KEY(forum_id)
-    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE SET NULL,
+    PRIMARY KEY(forumID)
+    FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE SET NULL,
 )
+
+CREATE TABLE MembuatForum(
+    forumID INTEGER NOT NULL,
+    userID INTEGER NOT NULL,
+    appID INTEGER NOT NULL,
+    PRIMARY KEY (forumID, userID, appID),
+    FOREIGN KEY (forumID) REFERENCES Forum(forumID) ON DELETE CASCADE,
+    FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE,
+    FOREIGN KEY (appID) REFERENCES Apps(appID) ON DELETE CASCADE,
+)
+
 CREATE TABLE Post(
-    post_id INTEGER NOT NULL AUTOINCREMENT,
-    forum_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    judul VARCHAR(255) NOT NULL,
+    postID INTEGER NOT NULL AUTOINCREMENT,
+    forumID INTEGER NOT NULL,
+    userID INTEGER NOT NULL,
+    konten_post VARCHAR(255) NOT NULL,
     waktu_pembuatan_post date NOT NULL,
-    PRIMARY KEY(post_id),
-    FOREIGN KEY (forum_id) REFERENCES Forum(forum_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,   
+    PRIMARY KEY(postID),
+    FOREIGN KEY (forumID) REFERENCES Forum(forumID) ON DELETE CASCADE,
+    FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE,   
 )
 
 CREATE TABLE Vote(
-    forum_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL,
-    PRIMARY KEY (forum_id, user_id),
-    FOREIGN KEY (forum_id) REFERENCES Forum(forum_id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES User(user_id) ON DELETE CASCADE,
+    forumID INTEGER NOT NULL,
+    userID INTEGER NOT NULL,
+    jenis_vote ENUM("UPVOTE", "DOWNVOTE"),
+    PRIMARY KEY (forumID, userID),
+    FOREIGN KEY (forumID) REFERENCES Forum(forumID) ON DELETE CASCADE,
+    FOREIGN KEY (userID) REFERENCES User(userID) ON DELETE CASCADE,
 )
 
 CREATE TABLE DLC(
-    game_id INTEGER NOT NULL,
-    judul VARCHAR(255) NOT NULL,
-    harga CHAR(10) NOT NULL DEFAULT 0,
-    tanggal_peluncuran date NOT NULL,
-    PRIMARY KEY(game_id),
-    FOREIGN KEY (game_id) REFERENCES VideoGames(game_id) ON DELETE CASCADE,
+    dlcID INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+    gameID INTEGER NOT NULL,
+    FOREIGN KEY (gameID) REFERENCES VideoGames(gameID) ON DELETE CASCADE,
+)
+
+CREATE TABLE DLCDetail(
+    dlcID INTEGER NOT NULL,
+    judul VARCHAR(50) NOT NULL,
+    harga CHAR(10) NOT NULL DEFAULT '0',
+    tanggal_peluncuran date,
+    PRIMARY KEY(dlcID),
+    FOREIGN KEY (dlcID) REFERENCES DLC(dlcID) ON DELETE CASCADE,
 )
 
 CREATE TABLE Award(
-    game_id INTEGER,
+    awardID INTEGER PRIMARY KEY NOT NULL AUTOINCREMENT,
+    gameID INTEGER,
     kategori VARCHAR(20) NOT NULL,
-    PRIMARY KEY(game_id),
-    FOREIGN KEY (game_id) REFERENCES VideoGames(game_id) ON DELETE SET NULL,
+    FOREIGN KEY (gameID) REFERENCES VideoGames(gameID) ON DELETE SET NULL,
 )
 
 CREATE TABLE Genre(
-    game_id INTEGER,
+    gameID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     genre VARCHAR(20) NOT NULL,
-    PRIMARY KEY(game_id),
-    FOREIGN KEY (game_id) REFERENCES VideoGames(game_id) ON DELETE CASCADE,
+    FOREIGN KEY (gameID) REFERENCES VideoGames(gameID) ON DELETE CASCADE,
 )
